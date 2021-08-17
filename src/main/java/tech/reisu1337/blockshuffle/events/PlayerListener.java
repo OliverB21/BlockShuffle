802870be69ec1af530ada6381e188c90566807ab
reisu1337 <73112598+reisu1337@users.noreply.github.com>
2021-08-17 18:48:35 +0100

    Added support for multiple rounds, called 0.2

--- src/main/java/tech/reisu1337/blockshuffle/events/PlayerListener.java
+++ src/main/java/tech/reisu1337/blockshuffle/events/PlayerListener.java
@@ -1,7 +1,6 @@
 package tech.reisu1337.blockshuffle.events;
 
-//import org.apache.commons.lang.WordUtils;
-
+import com.google.common.collect.Sets;
 import org.apache.commons.lang.WordUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -15,13 +14,18 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import tech.reisu1337.blockshuffle.BlockShuffle;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class PlayerListener implements Listener {
-    private final Map<UUID, Material> userMaterialMap = new HashMap<>();
+    private final Map<UUID, Material> userMaterialMap = new ConcurrentHashMap<>();
+    private final Set<UUID> completedUsers = Sets.newConcurrentHashSet();
+    private final Set<UUID> usersInGame = Sets.newConcurrentHashSet();
     private final Random random = new Random();
     private final List<Material> materials;
     private final BlockShuffle plugin;
+    private int ticksInRound = 600;
+    private int scheduledTask;
 
     public PlayerListener(YamlConfiguration settings, BlockShuffle plugin) {
         this.materials = settings.getStringList("materials").stream().map(Material::getMaterial).collect(Collectors.toList());
@@ -31,13 +35,52 @@ public class PlayerListener implements Listener {
     public void startGame() {
         plugin.setRoundWon(false);
         for (Player player : Bukkit.getOnlinePlayers()) {
+            this.usersInGame.add(player.getUniqueId());
+        }
+        this.nextRound();
+    }
+
+    private void nextRound() {
+        if (this.ticksInRound != 600) {
+            if (this.completedUsers.size() == 0) {
+                Bukkit.broadcastMessage(createWinnerMessage());
+                this.ticksInRound = 600;
+                this.userMaterialMap.clear();
+                this.usersInGame.clear();
+                this.plugin.setInProgress(false);
+                return;
+            } else {
+                for (UUID uuid : this.usersInGame) {
+                    if (!this.completedUsers.contains(uuid)) {
+                        usersInGame.remove(uuid);
+                        Bukkit.getPlayer(uuid).sendMessage("You failed haha lol");
+                    }
+                }
+            }
+            this.completedUsers.clear();
+        }
+        for (UUID uuid : this.usersInGame) {
+            Player player = Bukkit.getPlayer(uuid);
             Material randomBlock = this.getRandomMaterial();
             String playerOnBlock2 = randomBlock.toString().replaceAll("_", " ");
             playerOnBlock2 = WordUtils.swapCase(playerOnBlock2).toLowerCase(Locale.ROOT);
             playerOnBlock2 = WordUtils.capitalize(playerOnBlock2);
-            this.userMaterialMap.put(player.getUniqueId(), randomBlock);
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6<BlockShuffle> " + "&4" + player.getName() + "&f, you need to stand on &d" + playerOnBlock2));
+            this.userMaterialMap.put(uuid, randomBlock);
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6<BlockShuffle> " + "&4" + player.getName() + ",&f you have " + this.ticksInRound / 1200 + " mins to stand on &d" + playerOnBlock2));
         }
+        this.scheduledTask = Bukkit.getScheduler().scheduleSyncDelayedTask(this.plugin, ()->{
+            this.nextRound();
+        }, this.ticksInRound);
+        this.ticksInRound -= 300;
+    }
+
+    private String createWinnerMessage() {
+        //The game is over! The winner(s) are A, B, C etc
+        StringJoiner stringJoiner = new StringJoiner(", ", "The game is over, the winners are: ", "!");
+        for (UUID uuid : this.usersInGame) {
+            stringJoiner.add(Bukkit.getPlayer(uuid).getName());
+        }
+        return stringJoiner.toString();
     }
 
     private Material getRandomMaterial() {
@@ -51,12 +94,16 @@ public class PlayerListener implements Listener {
         Material playerOnBlock = player.getLocation().getBlock().getRelative(BlockFace.DOWN).getBlockData().getMaterial();
         if (this.userMaterialMap.get(player.getUniqueId()) == playerOnBlock) {
             if (!this.plugin.isRoundWon()) {
-                plugin.getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&6<BlockShuffle> " + "&2" + player.getName() + "&f has won!"));
-                plugin.setRoundWon(true);
+                plugin.getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&6<BlockShuffle> " + "&2" + player.getName() + "&f has found their block!"));
+                this.completedUsers.add(player.getUniqueId());
             }
             this.userMaterialMap.remove(event.getPlayer().getUniqueId());
             if (this.userMaterialMap.size() == 0) {
                 this.plugin.setInProgress(false);
+            }
+            if (this.completedUsers.size() == this.usersInGame.size()) {
+                Bukkit.getScheduler().cancelTask(this.scheduledTask);
+                this.nextRound();
             }
         }
         //playerOnBlock2 = playerOnBlock.replaceAll("_", " ");
